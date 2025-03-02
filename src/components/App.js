a57807268d31c3f57c1fb43721120ea0077bab78
Adel Mahmoud <dolacalisthenicsw@gmail.com>
2025-03-02 12:50:58 +0200

    add file App.css

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,17 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  const styles = { color: "red", fontSize: "44px" };
-  const myName = "Adel";
-  const names = ["Adel", "Dola", "M7"];
   return (
-    <fragent>
-      <h1>My React App</h1>
-      <h2>My React App</h2>
-      <h3>My React App</h3>
-      <h4>My React App</h4>
-      <p style={styles}>Hello World</p>
-      <p style={styles}>{myName === "Adel" ? "Hello Adel" : "Hello There"}</p>
-      <p style={styles}>{myName === "Dola" ? "Hello Adel" : "Hello There"}</p>
-      <div>
-        Names :
-        {names.map((el) => (
-          <p>{el}</p>
-        ))}
+    <div className="min-container">
+      <div className="cardWeapper">
+        <div>Name:Adel</div>
+        <div>Age: 26</div>
+        <div>Country: Egypt</div>
+        <div>Job: Software Developer</div>
+        <div>Phone :01064620610</div>
       </div>
-      <h5>My React App</h5>
-      <h6>My React App</h6>
-    </fragent>
+    </div>
   );
 }
 
